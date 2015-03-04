bb88a494a95631137ff42cf8f9ddf4c3ed73deb4
陈 豪 <chen@chenmatoMacBook-Air.local>
2015-03-04 12:12:31 +0800

    change version to 1.1.0

---
+++ server script/ios.py
@@ -0,0 +1,11 @@
+#/bin/env python
+# -*-coding:utf8-*-
+'''
+# Any issues or improvements please contact jacob-chen@iotwrt.com
+'''
+import urllib  
+
+
+if __name__ == '__main__':
+
+	pass
--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-const char *__version__ = "1.0.0";
+const char *__version__ = "1.1.0";
 const char *__release__ = "beta";
 const char *__revision__ = "$Id$";
 
